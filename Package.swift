8d6867214b6c3c19dc895b3c831840cc8e774ab9
Konrad Feiler <K_W_F@web.de>
2017-10-01 13:58:36 +0200

    update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,8 +12,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/Bersaelor/KDTree", .upToNextMinor(from: "1.0.2")),
-//        .package(url: "https://github.com/Bersaelor/SwiftyHYGDB", .upToNextMinor(from: "0.5.2")),
-        .package(url: "../SwiftyHYGDB", .upToNextMinor(from: "0.6.0")),
+        .package(url: "https://github.com/Bersaelor/SwiftyHYGDB", .upToNextMinor(from: "0.6.0")),
         .package(url: "https://github.com/IBM-Swift/Kitura.git", .upToNextMinor(from: "1.7.8")),
         .package(url: "https://github.com/IBM-Swift/HeliumLogger.git", .upToNextMinor(from: "1.7.1")),
         .package(url: "https://github.com/IBM-Swift/Kitura-StencilTemplateEngine.git", .upToNextMinor(from: "1.8.3"))
--- Sources/StarsOnKitura/Star+DataDictionary.swift
+++ Sources/StarsOnKitura/Star+DataDictionary.swift
@@ -17,10 +17,6 @@ extension RadialStar {
             return [:]
         }
         
-        let emptyToNull: (String) -> Any = { string in
-            return string.isEmpty ? NSNull() : string
-        }
-        
         // so long as Kitura uses SwiftyJson's LclJSONSerialization,
         // JSON data should not contain Int, or Optionals
         return [
@@ -30,14 +26,14 @@ extension RadialStar {
             "hip_id": data.hip_id != -1 ? Int(data.hip_id) : NSNull(),
             "hd_id": data.hd_id != -1 ? Int(data.hd_id) : NSNull(),
             "hr_id": data.hr_id != -1 ? Int(data.hr_id) : NSNull(),
-            "gl_id": emptyToNull(data.getGlId()),
-            "bayer_flamstedt": emptyToNull(data.getBayerFlamstedt()),
-            "properName": emptyToNull(data.getProperName()),
+            "gl_id": data.getGlId() ?? NSNull(),
+            "bayer_flamstedt": data.getBayerFlamstedt() ?? NSNull(),
+            "properName": data.getProperName() ?? NSNull(),
             "distance": data.distance,
             "rv": data.rv,
             "mag": data.mag,
             "absmag": data.absmag,
-            "spectralType": emptyToNull(data.getSpectralType()),
+            "spectralType": data.getSpectralType() ?? NSNull(),
             "colorIndex": data.colorIndex
         ]
     }
